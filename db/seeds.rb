393ffede5265f308360e0dc5e0aa88c094998cef
sam13008 <ikherbane.samir@gmail.com>
2018-04-30 12:14:07 +0200

    salut

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |index|
+  user = Movie.create(title: "Nom#{index}", release_year: "year#{index}", director: "Nom#{index}" )
+
+end
